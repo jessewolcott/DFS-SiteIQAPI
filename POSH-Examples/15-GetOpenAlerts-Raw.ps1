bb9cb9893cf12735f226303a896e3da6c14b056b
Jesse Wolcott <jesse.wolcott@mac.com>
2026-02-24 18:26:40 -0500

    flow rate expansion

--- POSH-Examples/15-GetOpenAlerts-Raw.ps1
+++ POSH-Examples/15-GetOpenAlerts-Raw.ps1
@@ -47,14 +47,40 @@ $openAlerts = foreach ($t in $tickets) {
     }
 }
 
+# Classify alerts by error type
+$NoTransaction = [System.Collections.Generic.List[object]]::new()
+$LowFlow       = [System.Collections.Generic.List[object]]::new()
+$Unclassified  = [System.Collections.Generic.List[object]]::new()
+
 foreach ($alert in $openAlerts) {
     switch -Wildcard ($alert.Error) {
-        'No Transaction*' { $NoTransaction.Add($alert) | Sort-Object SiteName }
-        'Grade*'          { $LowFlow.Add($alert)| Sort-Object SiteName  }
-        default           { $Unclassified.Add($alert) | Sort-Object SiteName  }
+        'No Transaction*' {
+            $NoTransaction.Add($alert)
+            break
+        }
+        'Grade*' {
+            $parts   = $alert.Error -split '\s*:\s*'
+            $siteNum = ($alert.SiteName -replace '-.*$').Trim()
+            $LowFlow.Add([PSCustomObject]@{
+                TicketID        = $alert.TicketID
+                SiteID          = $siteNum.PadLeft(4, '0')
+                SiteName        = $alert.SiteName
+                Component       = $alert.Component
+                Dispenser       = $alert.Dispenser
+                Product         = $parts[0].Trim()
+                FlowRate        = $parts[1].Trim()
+                Error           = $alert.Error
+                FuelingPosition = $alert.FuelingPosition
+                AlertOpened     = $alert.AlertOpened
+            })
+            break
+        }
+        default {
+            $Unclassified.Add($alert)
+        }
     }
 }
 
-$NoTransaction
-$LowFlow
-$Unclassified 
+$NoTransaction | Sort-Object SiteName
+$LowFlow       | Sort-Object SiteName
+$Unclassified  | Sort-Object SiteName
